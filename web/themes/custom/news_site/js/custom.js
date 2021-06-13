741831c9a382af0e89b64e3068aa6f835a2ee52a
Petro <mccolaichuk@gmail.com>
2021-06-13 19:55:13 +0300

    Added theme.

---
+++ web/themes/custom/news_site/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.news_site = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
